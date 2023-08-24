1cf47fd0e35919612e155b2b4112537ddcece8bb
Mateoferrer66 <mateoferrer66@gmail.com>
2023-08-24 13:51:12 -0500

    cursointroreact

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './react512.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edita el archivo <code>src/App.js</code> y guarda para recargar.
+        </p>
+        <a> Aprendamos React </a>          
+      </header>
+    </div>
+  );
+}
+
+export default App;
